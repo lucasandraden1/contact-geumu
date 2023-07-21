8db336437bd5dbbd0ee73a96917960db46b46886
Lucas Andrade <lucas.andrade@agencian1.com.br>
2023-07-21 13:20:27 -0300

    feat add hotjar

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { GlobalStyle } from './styles.js'
+import { GlobalStyle } from './styles.js';
+/* import Hotjar from '@hotjar/browser';
+
+const siteId = 3583411;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion); */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
